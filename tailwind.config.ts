d4b741ecc5d83d218759d26b8a7cd74a2cd2f03e
Hakushin <kpx1164@gmail.com>
2024-06-06 18:57:44 +0700

    update packages and colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,29 @@
 import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#ffffff', // Default (light mode)
+          dark: '#000000'  // Dark mode
+        },
+        secondary: {
+          DEFAULT: '#f2f2f8', // Default (light mode)
+          dark: '#1c1c1e'    // Dark mode
+        },
+        tertiary: {
+          DEFAULT: '#ffffff',   // Default (light mode)
+          dark: '#2c2c2e'    // Dark mode
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,6 +31,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
+
 export default config;
